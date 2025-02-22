85bd36305bfb867dc321466d05b2ea8e486c6f21
anamsilva1981 <ana.msilva@hotmail.com>
2025-02-22 05:43:04 -0300

    feat: add project configuration files and refactor calculator script

--- scripts/controller/calcController.js
+++ scripts/controller/calcController.js
@@ -1,3 +1,33 @@
 class CalcController{
-    
+
+    constructor(){
+        this._displayCalc = "0";
+        this._currentDate;
+
+        this.initialize();
+    }
+
+    initialize(){
+        let displayCalcEl =  document.querySelector("#display");
+        let dateEl = document.querySelector("#data");
+        let timeRl = document.querySelector("#hora");
+
+        displayCalcEl.innerHTML = "4567";
+    }
+
+    get displayCalc(){
+        return this._displayCalc;
+    }
+
+    set displayCalc(valor){
+        this._displayCalc = valor
+    }
+
+    get dataAtual(){
+        return this._currentDate;
+    }
+
+    set dataAtual(data){
+        this._currentDate = data
+    }
 }
